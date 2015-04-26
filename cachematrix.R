426a1af1ae164a36e9302231946b5c72a5d2139d
Rick Hubbard <rick.hubbard.advisor@gmail.com>
2015-04-25 18:41:39 -0700

    Second
    A little bit of coding. Not the hard parts (solve), yet.

--- cachematrix.R
+++ cachematrix.R
@@ -5,8 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+     m <- NULL
+     set <- function(y){
+          x <<- y
+          m <<- NULL
+     }
+     get <- function()x
+     makeinversion <- x
+     getinversion <- x
+     list(set = set, get=get, makeinversion=makeinversion,getinversion=getinversion)
+     
 }
-
 
 ## cacheSolve will return an inversion of an R object created by makeCacheMatrix.
 ##      The cached version of the inverted matrix will be returned if there is no change in the inputted matrix (from one invocation to another).
@@ -14,4 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     m <- x$getinversion()
+     if(!is.null(m)){
+          message("Returning Cached Version of Inverted Matrix")
+          return(m)
+     }
+     matrixtoinvert <- x$get()
+     m <- makeinversion(matrixtoinvert, ...)
+     m
 }
